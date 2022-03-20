d999f4436cd56a85996eb39343ec6db03769f13d
Dayanne <day_garcia_anne@hotmail.com>
2022-03-20 19:56:01 -0300

    foo

--- projetoevent/settings.py
+++ projetoevent/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = 'django-insecure-)r58hs22ar2ytc(!_=z48#pw67r6@3ar6(&cn&js%pazrw$a#t'
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'foo')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = int(os.environ.get("DEBUG", default=1))
