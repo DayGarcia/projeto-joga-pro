91074e9b2cc78dd61f6c832f82cd34f6e8adda0d
Ives Samuel <ivessamuel64@gmail.com>
2022-03-20 20:07:34 -0300

    esqueceu do campo na model '-'

--- projetoevent/models.py
+++ projetoevent/models.py
@@ -3,9 +3,10 @@ from django.db import models
 
 class Event(models.Model):
     id = models.AutoField(primary_key=True)
+    is_running = models.IntegerField()
 
     def __str__(self):
-        return self.name
+        return self.is_running
 
 
 class Ticket(models.Model):
