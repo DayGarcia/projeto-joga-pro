8d21bad33dcd7adebf6f22b7b8ffe4935878ab4e
Dayanne <day_garcia_anne@hotmail.com>
2022-04-11 01:12:18 -0300

    null safeoperator because python fucks at array (dict) whatever indexes

--- projetoevent/helpers/event_query.py
+++ projetoevent/helpers/event_query.py
@@ -2,5 +2,9 @@ from projetoevent.models import Event
 
 
 def get_running_event_id():
-    return Event.objects.filter(
-        is_running=1).values_list('id', flat=True)[0]
+    running_event_id = Event.objects.filter(
+        is_running=1).values_list('id', flat=True)
+    if len(running_event_id) > 0:
+        return running_event_id[0]
+    else:
+        return 0
