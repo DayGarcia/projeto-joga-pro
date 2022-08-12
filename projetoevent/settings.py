0d33935d63ad857bf32d08cf8869bb8de9104edc
Dayanne <day_garcia_anne@hotmail.com>
2022-08-11 23:08:47 -0300

    poggers config complete

--- projetoevent/settings.py
+++ projetoevent/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'foo')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = int(os.environ.get("DEBUG", default=True))
+DEBUG = os.environ.get("DEBUG", default=1)
 
 # print(os.environ.get("DJANGO_ALLOWED_HOSTS"))
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
